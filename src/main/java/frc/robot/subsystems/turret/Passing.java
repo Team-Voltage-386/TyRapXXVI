97a935ed89d891950790dd8929958e8cf204a7f8
Aaaaakshay101 <akshayvenkat12@gmail.com>
2026-04-01 20:09:17 -0400

    passing tof

--- src/main/java/frc/robot/subsystems/turret/Passing.java
+++ src/main/java/frc/robot/subsystems/turret/Passing.java
@@ -18,22 +18,22 @@ public class Passing extends ShootingConstants {
     launchHoodAngleMap.put(4.5, Rotation2d.fromDegrees(40.0));
     launchHoodAngleMap.put(6.0, Rotation2d.fromDegrees(40.0));
     launchHoodAngleMap.put(8.0, Rotation2d.fromDegrees(40.0));
-    // launchHoodAngleMap.put(10.0, Rotation2d.fromDegrees(40.0));
+    // launchHoodAngleMap.put(10.0, Rotation2d.fromDegrees(40.0)); probably not needed
     // test 10
 
-    // get TOF
+    // 
     launchFlywheelSpeedMap.put(2.0, 2000.0); // RPM
     launchFlywheelSpeedMap.put(3.0, 2000.0); // RPM
     launchFlywheelSpeedMap.put(4.5, 2550.0);
     launchFlywheelSpeedMap.put(6.0, 3000.0);
     launchFlywheelSpeedMap.put(8.0, 3750.0);
-    // launchFlywheelSpeedMap.put(10.0, 4500.0);
+    
 
-    timeOfFlightMap.put(1.669, 0.88);
-    timeOfFlightMap.put(2.233, 0.9275);
-    timeOfFlightMap.put(2.823, 1.007);
-    timeOfFlightMap.put(3.782, 1.057);
-    timeOfFlightMap.put(4.75, 1.167);
-    timeOfFlightMap.put(5.0, 1.197);
+    timeOfFlightMap.put(3.11, 0.92);
+    timeOfFlightMap.put(4.0, 0.96);
+    timeOfFlightMap.put(5.5, 1.03);
+    timeOfFlightMap.put(8.1, 1.55);
+    timeOfFlightMap.put(9.11, 1.61);
+    
   }
 }
