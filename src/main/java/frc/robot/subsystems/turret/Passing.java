34bdf7af6d3b45231b58de7288dbd1be022e8007
Joseph Argento <joseph.m.argento@gmail.com>
2026-04-03 18:34:55 -0400

    Cleaned up comments

--- src/main/java/frc/robot/subsystems/turret/Passing.java
+++ src/main/java/frc/robot/subsystems/turret/Passing.java
@@ -11,23 +11,19 @@ public class Passing extends ShootingConstants {
     phaseDelay = 0.03;
 
     // max is 62 degrees
-
-    // need to
     launchHoodAngleMap.put(2.0, Rotation2d.fromDegrees(52.0));
     launchHoodAngleMap.put(3.0, Rotation2d.fromDegrees(50.0));
     launchHoodAngleMap.put(4.5, Rotation2d.fromDegrees(40.0));
     launchHoodAngleMap.put(6.0, Rotation2d.fromDegrees(40.0));
     launchHoodAngleMap.put(8.0, Rotation2d.fromDegrees(40.0));
-    // launchHoodAngleMap.put(10.0, Rotation2d.fromDegrees(40.0)); probably not needed
-    // test 10
-
-    // 
+    // launchHoodAngleMap.put(10.0, Rotation2d.fromDegrees(40.0));
+  
     launchFlywheelSpeedMap.put(2.0, 2000.0); // RPM
     launchFlywheelSpeedMap.put(3.0, 2000.0); // RPM
     launchFlywheelSpeedMap.put(4.5, 2550.0);
     launchFlywheelSpeedMap.put(6.0, 3000.0);
     launchFlywheelSpeedMap.put(8.0, 3750.0);
-    
+    // launchFlywheelSpeedMap.put(10.0, 4500.0);
 
     timeOfFlightMap.put(3.11, 0.92);
     timeOfFlightMap.put(4.0, 0.96);
