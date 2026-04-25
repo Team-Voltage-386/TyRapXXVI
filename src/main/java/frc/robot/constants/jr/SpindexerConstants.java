fd6297b265b844897535e1a2651eeddfed74887c
skylerludovice99 <ludoviceskyler@gmail.com>
2026-04-25 14:13:55 -0400

    removed antijam motor

--- src/main/java/frc/robot/constants/jr/SpindexerConstants.java
+++ src/main/java/frc/robot/constants/jr/SpindexerConstants.java
@@ -3,8 +3,6 @@ package frc.robot.constants.jr;
 public class SpindexerConstants {
   public static final int SPINDEXER_MOTOR_CAN_ID = 30;
   public static final int FEEDER_MOTOR_CAN_ID = 31;
-  public static final int ANTIJAM_MOTOR_CAN_ID = 41;
   public static final double SPINDEXER_MOTOR_VOLTAGE = -5.0;
-  public static final double ANTIJAM_VOLTAGE = 6.0; // needs further testing
   public static final double FEEDER_MOTOR_VOLTAGE = 0.95 * 12;
 }
